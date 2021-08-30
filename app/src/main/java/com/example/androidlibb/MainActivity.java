16921308cbe1b29e930a3d003dbfc1d1e9419544
abhijeet.patil@flairlabs.com <FlairLabs@2020>
2021-08-30 10:13:08 +0530

    first commit

--- app/src/main/java/com/example/androidlibb/MainActivity.java
+++ app/src/main/java/com/example/androidlibb/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.androidlibb;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.dalcapture.Calculate;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println(""+ new Calculate().addValues(10, 20));
     }
 }
--- dalcapture/src/main/java/com/example/dalcapture/Calculate.java
+++ dalcapture/src/main/java/com/example/dalcapture/Calculate.java
@@ -2,4 +2,8 @@ package com.example.dalcapture;
 
 public class Calculate {
 
+    public int addValues(int a, int b){
+        return a+b;
+    }
+
 }
