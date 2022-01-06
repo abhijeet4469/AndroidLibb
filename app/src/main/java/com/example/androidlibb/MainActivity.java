10ff2e8f84a7b82f4deb7f06dc1be43a4599a35a
abhijeet.patil@flairlabs.com <FlairLabs@2020>
2022-01-06 14:37:42 +0530

    first commit

--- app/src/main/java/com/example/androidlibb/MainActivity.java
+++ app/src/main/java/com/example/androidlibb/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         System.out.println(""+ new Calculate().addValues(10, 20));
     }
 }
